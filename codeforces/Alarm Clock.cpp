d869d5fd43251815e18e1bdbb2f3067eb8000d83
shandilya <shubhamshandilya0598@gmail.com>
2020-05-18 15:44:59 +0530

    added Alarm clock.cpp file

---
+++ codeforces/Alarm Clock.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long int
+#define KILL ios_base::sync_with_stdio(false);
+#define HER cin.tie(NULL);
+#define NOW cout.tie(NULL);
+#define fori(i,a,b) for(i=a;i<b;i++)
+#define forreverse(i,a,b) for(i=a;i>=b;i--)
+#define full(x) x.begin(),x.end()
+#define go(t) while(t--)
+#define endl cout<<"\n"
+#define pb push_back
+#define pob pop_back
+#define mp make_pair
+#define M 1000000007
+
+int main()
+{
+	/*
+	#ifndef ONLINE_JUDGE
+	freopen("inputCF.txt","r",stdin);
+    freopen("outputCF.txt","w",stdout);
+	#endif
+	
+	#ifndef ONLINE_JUDGE
+	freopen("inputHR.txt","r",stdin);
+    freopen("outputHR.txt","w",stdout);
+	#endif
+
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+
+	#ifndef ONLINE_JUDGE
+	freopen("inputCC.txt","r",stdin);
+    freopen("outputCC.txt","w",stdout);
+	#endif
+
+	#ifndef ONLINE_JUDGE
+	freopen("inputGFG.txt","r",stdin);
+    freopen("outputGFG.txt","w",stdout);
+	#endif
+	*/
+	#ifndef ONLINE_JUDGE
+	freopen("inputCF.txt","r",stdin);
+    freopen("outputCF.txt","w",stdout);
+	#endif
+	KILL HER NOW
+	LL t;
+	cin>>t;
+	go(t)
+	{
+		LL a,b,c,d,cnt,rem,rem1,sum;
+		cin>>a>>b>>c>>d;
+		if(b>=a) cout<<b<<"\n";
+		else if(b<a && d>=c) cout<<-1<<"\n";
+		else if(b<a && d<c)
+		{
+			sum=b;
+			rem=a-b;
+			rem1=c-d;
+			cnt=rem/rem1;
+			if(rem%rem1!=0) cnt++;
+			sum+=cnt*c;
+			cout<<sum<<"\n";
+		}
+	}
+	return 0;
+}
