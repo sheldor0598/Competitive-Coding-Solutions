da9c9630f1d705602d9af469ac7f89022b01914a
shandilya <shubhamshandilya0598@gmail.com>
2020-05-19 18:08:02 +0530

    added ALEXNUM.cpp file

---
+++ codechef/ALEXNUM.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long int
+#define KILL ios_base::sync_with_stdio(false);
+#define HER cin.tie(NULL);
+#define gof1(i,a,b) for(i=a;i<b;i++)
+#define gof2(i,a,b) for(i=a;i<=b;i++)
+#define go(t) while(t--)
+#define take cin
+#define in >>
+#define give cout
+#define out <<
+#define endl "\n"
+
+int main()
+{
+	/*
+	#ifndef ONLINE_JUDGE
+	freopen("inputCF.txt","r",stdin);
+    freopen("outputCF.txt","w",stdout);
+	#endif
+	
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+
+	#ifndef ONLINE_JUDGE
+	freopen("inputCC.txt","r",stdin);
+    freopen("outputCC.txt","w",stdout);
+	#endif
+
+	#ifndef ONLINE_JUDGE
+	freopen("inputGFG.txt","r",stdin);
+    freopen("outputGFG.txt","w",stdout);
+	#endif
+	*/
+	#ifndef ONLINE_JUDGE
+	freopen("inputCC.txt","r",stdin);
+    freopen("outputCC.txt","w",stdout);
+	#endif
+	KILL HER
+	LL t;
+	take in t;
+	go(t)
+	{
+		LL n;
+		take in n;
+		LL A[n],i;
+        gof1(i,0,n)
+            take in A[i];
+        sort(A,A+n);
+        LL cnt=0;
+        gof1(i,0,n)
+	        cnt+=n-i-1;
+        give out cnt out endl;
+	}
+	return 0;
+}
