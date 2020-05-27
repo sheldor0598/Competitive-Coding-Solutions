2dfb0c12b75615cba733a2fe73072832dc8d3a47
shandilya <shubhamshandilya0598@gmail.com>
2020-05-27 13:44:37 +0530

    added 1 new program in codeforces

---
+++ codeforces/Park Lighting.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long int
+#define KILL ios_base::sync_with_stdio(false);
+#define HER cin.tie(NULL);
+#define NOW cout.tie(NULL);
+#define fori(i,a,b) for(i=a;i<b;i++)
+#define forreverse(i,a,b) for(i=a;i>=b;i--)
+#define full(x) x.begin(),x.end()
+#define go(t) while(t--)
+#define endl cout<<"\n"
+#define pb push_back
+#define pob pop_back
+#define mp make_pair
+#define M 1000000007
+
+int main()
+{
+	/*
+	#ifndef ONLINE_JUDGE
+	freopen("inputCF.txt","r",stdin);
+    freopen("outputCF.txt","w",stdout);
+	#endif
+	
+	#ifndef ONLINE_JUDGE
+	freopen("inputHR.txt","r",stdin);
+    freopen("outputHR.txt","w",stdout);
+	#endif
+
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+
+	#ifndef ONLINE_JUDGE
+	freopen("inputCC.txt","r",stdin);
+    freopen("outputCC.txt","w",stdout);
+	#endif
+
+	#ifndef ONLINE_JUDGE
+	freopen("inputGFG.txt","r",stdin);
+    freopen("outputGFG.txt","w",stdout);
+	#endif
+	*/
+	#ifndef ONLINE_JUDGE
+	freopen("inputCF.txt","r",stdin);
+    freopen("outputCF.txt","w",stdout);
+	#endif
+	KILL HER NOW
+	LL t;
+	cin>>t;
+	go(t)
+	{
+		LL r,c,cnt=0,d;
+		cin>>r>>c;
+		cnt=r * (c/2);
+		if(c%2!=0)
+		{
+			if(r%2==0) cnt+=(r/2);
+			else cnt+=(r/2)+1;
+		}
+        cout<<cnt<<"\n";
+	}
+	return 0;
+}
