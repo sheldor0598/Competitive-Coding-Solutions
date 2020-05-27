787957a5524094d9c5bed973db3e207ca61cfd73
shandilya <shubhamshandilya0598@gmail.com>
2020-05-27 13:43:30 +0530

    added 1 new program in codeforces

---
+++ codeforces/Maria Breaks the Self-isolation.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long int
+#define KILL ios_base::sync_with_stdio(false);
+#define HER cin.tie(NULL);
+#define NOW cout.tie(NULL);
+#define fori(i,a,b) for(i=a;i<b;i++)
+#define forreverse(i,a,b) for(i=a;i>=b;i--)
+#define full(x) x.begin(),x.end()
+#define go(t) while(t--)
+#define endl cout<<"\n"
+#define pb push_back
+#define pob pop_back
+#define mp make_pair
+#define M 1000000007
+
+int main()
+{
+	/*
+	#ifndef ONLINE_JUDGE
+	freopen("inputCF.txt","r",stdin);
+    freopen("outputCF.txt","w",stdout);
+	#endif
+	
+	#ifndef ONLINE_JUDGE
+	freopen("inputHR.txt","r",stdin);
+    freopen("outputHR.txt","w",stdout);
+	#endif
+
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+
+	#ifndef ONLINE_JUDGE
+	freopen("inputCC.txt","r",stdin);
+    freopen("outputCC.txt","w",stdout);
+	#endif
+
+	#ifndef ONLINE_JUDGE
+	freopen("inputGFG.txt","r",stdin);
+    freopen("outputGFG.txt","w",stdout);
+	#endif
+	*/
+	#ifndef ONLINE_JUDGE
+	freopen("inputCF.txt","r",stdin);
+    freopen("outputCF.txt","w",stdout);
+	#endif
+	KILL HER NOW
+	LL t;
+	cin>>t;
+	go(t)
+	{
+		LL n;
+		cin>>n;
+		LL A[n+1],i,cnt=1;
+        fori(i,1,n+1) cin>>A[i];
+        sort(A+1,A+n+1);
+        forreverse(i,n,1) if(A[i]<=i) { cnt+=i; break; }
+        cout<<cnt<<"\n";
+	}
+	return 0;
+}
