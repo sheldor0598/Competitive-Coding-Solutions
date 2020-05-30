fb77d35666b85e5b80303d50eccf20b14133c5fb
shandilya <shubhamshandilya0598@gmail.com>
2020-05-30 10:22:55 +0530

    added 1 new program in codeforces

---
+++ codeforces/Tanya and Stairways.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long int
+#define KILL ios_base::sync_with_stdio(false);
+#define HER cin.tie(NULL);
+#define goflessthan(i,a,b) for(i=a;i<b;i++)
+#define gofequalto(i,a,b) for(i=a;i<=b;i++)
+#define gofminus(i,a,b) for(i=a;i>=b;i--)
+#define go(t) while(t--)
+#define endl cout<<"\n"
+#define pb push_back
+#define pob pop_back
+#define mp make_pair
+
+int main()
+{
+	/*
+	#ifndef ONLINE_JUDGE
+	freopen("inputCF.txt","r",stdin);
+    freopen("outputCF.txt","w",stdout);
+	#endif
+	
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+
+	#ifndef ONLINE_JUDGE
+	freopen("inputCC.txt","r",stdin);
+    freopen("outputCC.txt","w",stdout);
+	#endif
+
+	#ifndef ONLINE_JUDGE
+	freopen("inputGFG.txt","r",stdin);
+    freopen("outputGFG.txt","w",stdout);
+	#endif
+	*/
+	#ifndef ONLINE_JUDGE
+	freopen("inputCF.txt","r",stdin);
+    freopen("outputCF.txt","w",stdout);
+	#endif
+	KILL HER
+	LL n;
+	cin>>n;
+	LL A[n+1],i;
+    goflessthan(i,0,n) cin>>A[i];
+    A[i]=1;
+    cout<<count(A,A+n,1)<<"\n";
+    goflessthan(i,1,n+1) if(A[i]==1) cout<<A[i-1]<<" ";
+	return 0;
+}
