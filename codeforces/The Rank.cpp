1083d1e68aa9822f2e4d3d565bd1125add9171ad
shandilya <shubhamshandilya0598@gmail.com>
2020-06-06 14:58:39 +0530

    added 1 new program in codeforces

---
+++ codeforces/The Rank.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long int
+#define KILL ios_base::sync_with_stdio(false);
+#define HER cin.tie(NULL);
+#define goflessthan(i,a,b) for(i=a;i<b;i++)
+#define gofequalto(i,a,b) for(i=a;i<=b;i++)
+#define gofminus(i,a,b) for(i=a;i>=b;i--)
+#define go(t) while(t--)
+#define endl cout<<"\n"
+#define pb push_back
+#define pob pop_back
+#define mp make_pair
+
+int main()
+{
+	/*
+	#ifndef ONLINE_JUDGE
+	freopen("inputCF.txt","r",stdin);
+    freopen("outputCF.txt","w",stdout);
+	#endif
+	
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+
+	#ifndef ONLINE_JUDGE
+	freopen("inputCC.txt","r",stdin);
+    freopen("outputCC.txt","w",stdout);
+	#endif
+
+	#ifndef ONLINE_JUDGE
+	freopen("inputGFG.txt","r",stdin);
+    freopen("outputGFG.txt","w",stdout);
+	#endif
+	*/
+	#ifndef ONLINE_JUDGE
+	freopen("inputCF.txt","r",stdin);
+    freopen("outputCF.txt","w",stdout);
+	#endif
+	KILL HER
+	LL n;
+	cin>>n;
+	LL a,b,c,d,i,p=0;
+	multimap<LL,LL,greater<LL>>mp;
+    goflessthan(i,1,n+1)
+    {
+    	LL sum=0;
+    	cin>>a>>b>>c>>d;
+    	sum=a+b+c+d;
+    	mp.insert({sum,i});
+    }
+    for(auto x : mp)
+    {
+    	p++;
+    	if(x.second == 1)
+    	{
+    		cout<<p;
+    		break;
+    	}
+    }
+	return 0;
+}
