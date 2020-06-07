93b7be6c0d443672a2b831abbc4f83a280200d25
shandilya <shubhamshandilya0598@gmail.com>
2020-06-07 22:04:31 +0530

    added 1 new program in codeforces

---
+++ codeforces/Vitaly and Night.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long int
+#define KILL ios_base::sync_with_stdio(false);
+#define HER cin.tie(NULL);
+#define goflessthan(i,a,b) for(i=a;i<b;i++)
+#define gofequalto(i,a,b) for(i=a;i<=b;i++)
+#define gofminus(i,a,b) for(i=a;i>=b;i--)
+#define go(t) while(t--)
+#define endl cout<<"\n"
+#define pb push_back
+#define pob pop_back
+#define mp make_pair
+
+int main()
+{
+	/*
+	#ifndef ONLINE_JUDGE
+	freopen("inputCF.txt","r",stdin);
+    freopen("outputCF.txt","w",stdout);
+	#endif
+	
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+
+	#ifndef ONLINE_JUDGE
+	freopen("inputCC.txt","r",stdin);
+    freopen("outputCC.txt","w",stdout);
+	#endif
+
+	#ifndef ONLINE_JUDGE
+	freopen("inputGFG.txt","r",stdin);
+    freopen("outputGFG.txt","w",stdout);
+	#endif
+	*/
+	#ifndef ONLINE_JUDGE
+	freopen("inputCF.txt","r",stdin);
+    freopen("outputCF.txt","w",stdout);
+	#endif
+	KILL HER
+	LL n,m,cnt=0,i;
+	cin>>n>>m;
+	go(n)
+	{
+		LL p=2*m + 1;
+		LL A[p],i;
+        goflessthan(i,1,p)
+            cin>>A[i];
+        goflessthan(i,2,p)
+        {
+        	if(A[i]==1 || A[i-1]==1)
+        		cnt++;
+        	i++;
+        }
+	}
+	cout<<cnt;
+	return 0;
+}
