21ece259003e72f2dfdb5dbeaea1c39e5ee98590
shandilya <shubhamshandilya0598@gmail.com>
2020-06-17 12:42:38 +0530

    Round #650 Div.3 - B

---
+++ codeforces/Even Array.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long int
+#define KILL ios_base::sync_with_stdio(false);
+#define HER cin.tie(NULL);
+#define NOW cout.tie(NULL);
+#define fori(i,a,b) for(i=a;i<b;i++)
+#define forireverse(i,a,b) for(i=a;i>=b;i--)
+#define full(x) x.begin(),x.end()
+#define go(t) while(t--)
+#define endl cout<<"\n"
+#define pb push_back
+#define pob pop_back
+#define mp make_pair
+#define M 1000000007
+
+int main()
+{
+	/*
+	#ifndef ONLINE_JUDGE				#ifndef ONLINE_JUDGE				#ifndef ONLINE_JUDGE
+	freopen("inputCF.txt","r",stdin);	freopen("inputCC.txt","r",stdin);	freopen("input.txt","r",stdin);
+	freopen("outputCF.txt","w",stdout);	freopen("outputCC.txt","w",stdout); freopen("output.txt","w",stdout);
+	#endif 								#endif 								#endif
+
+	#ifndef ONLINE_JUDGE				#ifndef ONLINE_JUDGE
+	freopen("inputHR.txt","r",stdin);	freopen("inputGFG.txt","r",stdin);
+    freopen("outputHR.txt","w",stdout); freopen("outputGFG.txt","w",stdout);
+	#endif 								#endif
+	*/
+	
+	#ifndef ONLINE_JUDGE
+	freopen("inputCF.txt","r",stdin);
+	freopen("outputCF.txt","w",stdout);
+	#endif
+	KILL HER NOW
+	LL t=1;
+	cin>>t;
+	go(t)
+	{
+		LL n,cnt=0,j;
+		cin>>n;
+		LL A[n],i,ev=0,od=0;
+        fori(i,0,n)
+        {
+        	cin>>A[i];
+        	if(A[i]%2==0) ev++;
+        	else od++;
+        	if(i%2==0 && A[i]%2!=0) cnt++;
+        }
+        if((n%2==0 && ev!=od) || (n%2!=0 && (ev-od)!=1)) cout<<-1<<"\n";
+        else cout<<cnt<<"\n";
+	}
+	return 0;
+}
