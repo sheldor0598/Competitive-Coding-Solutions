fdbe964445e748cd63426b9c74a1c9f98b3f2a3c
shandilya <shubhamshandilya0598@gmail.com>
2020-06-20 17:43:15 +0530

    added 1 solution in codeforces

---
+++ codeforces/multiplication table.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long int
+#define KILL ios_base::sync_with_stdio(false);
+#define HER cin.tie(NULL);
+#define goflessthan(i,a,b) for(i=a;i<b;i++)
+#define gofequalto(i,a,b) for(i=a;i<=b;i++)
+#define gofminus(i,a,b) for(i=a;i>=b;i--)
+#define go(t) while(t--)
+#define take cin
+#define in >>
+#define give cout
+#define out <<
+#define endl "\n"
+#define pb push_back
+#define mp make_pair
+
+int main()
+{
+	/*
+	#ifndef ONLINE_JUDGE
+	freopen("inputCF.txt","r",stdin);
+    freopen("outputCF.txt","w",stdout);
+	#endif
+	
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+
+	#ifndef ONLINE_JUDGE
+	freopen("inputCC.txt","r",stdin);
+    freopen("outputCC.txt","w",stdout);
+	#endif
+
+	#ifndef ONLINE_JUDGE
+	freopen("inputGFG.txt","r",stdin);
+    freopen("outputGFG.txt","w",stdout);
+	#endif
+	*/
+	#ifndef ONLINE_JUDGE
+	freopen("inputCF.txt","r",stdin);
+    freopen("outputCF.txt","w",stdout);
+	#endif
+	KILL HER
+	LL n,x,i,j,cnt=0;
+	take in n in x;
+	gofequalto(i,1,n)
+	{
+		if(x%i != 0)
+			continue;
+		gofequalto(j,1,n)
+		{
+			if(x%j != 0)
+				continue;
+			if(i*j==x)
+				cnt++;
+		}
+	}
+	give out cnt;
+	return 0;
+}
