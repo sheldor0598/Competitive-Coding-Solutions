f3d0fc74e86759981b939700d7464426995f9105
shandilya <shubhamshandilya0598@gmail.com>
2020-06-22 12:06:36 +0530

    added 1 solution in codeforces

---
+++ codeforces/Lucky String.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long int
+#define KILL ios_base::sync_with_stdio(false);
+#define HER cin.tie(NULL);
+#define NOW cout.tie(NULL);
+#define fori(i,a,b) for(i=a;i<b;i++)
+#define forireverse(i,a,b) for(i=a;i>=b;i--)
+#define full(x) x.begin(),x.end()
+#define go(t) while(t--)
+#define endl cout<<"\n"
+#define pb push_back
+#define pob pop_back
+#define mp make_pair
+#define M 1000000007
+
+int main()
+{
+	/*
+	#ifndef ONLINE_JUDGE				#ifndef ONLINE_JUDGE				#ifndef ONLINE_JUDGE
+	freopen("inputCF.txt","r",stdin);	freopen("inputCC.txt","r",stdin);	freopen("input.txt","r",stdin);
+	freopen("outputCF.txt","w",stdout);	freopen("outputCC.txt","w",stdout); freopen("output.txt","w",stdout);
+	#endif 								#endif 								#endif
+
+	#ifndef ONLINE_JUDGE				#ifndef ONLINE_JUDGE
+	freopen("inputHR.txt","r",stdin);	freopen("inputGFG.txt","r",stdin);
+    freopen("outputHR.txt","w",stdout); freopen("outputGFG.txt","w",stdout);
+	#endif 								#endif
+	*/
+	
+	#ifndef ONLINE_JUDGE
+	freopen("inputCF.txt","r",stdin);
+	freopen("outputCF.txt","w",stdout);
+	#endif
+	KILL HER NOW
+	LL t=1;
+	go(t)
+	{
+		LL n,i;
+		cin>>n;
+		string str="",s="abcdefghijklmnopqrstuvwxyz";
+		LL p=1;
+		char A[n+1];
+		fill(A+1,A+n+1,'@');
+		A[1]='a';
+		for(i=5;i<=n;i+=4) A[i]='a';
+        fori(i,1,n+1)
+        {
+        	if(A[i-1]=='a') p=1;
+        	if(A[i]=='@') { A[i]=s[p]; p++; }
+        }
+        fori(i,1,n+1) cout<<A[i];
+        
+	}
+	return 0;
+}
