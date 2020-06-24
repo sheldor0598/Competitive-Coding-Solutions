6646224af33680275fb7cb3d10055db2fb87ea47
shandilya <shubhamshandilya0598@gmail.com>
2020-06-24 12:55:07 +0530

    added 1 solution in GeeksForGeeks

---
+++ GeeksForGeeks/Addition Under Modulo.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long int
+#define KILL ios_base::sync_with_stdio(false);
+#define HER cin.tie(NULL);
+#define NOW cout.tie(NULL);
+#define fori(i,a,b) for(i=a;i<b;i++)
+#define forireverse(i,a,b) for(i=a;i>=b;i--)
+#define full(x) x.begin(),x.end()
+#define go(t) while(t--)
+#define endl cout<<"\n"
+#define pb push_back
+#define pob pop_back
+#define mp make_pair
+#define M 1000000007
+
+int main()
+{
+	/*
+	#ifndef ONLINE_JUDGE				#ifndef ONLINE_JUDGE				#ifndef ONLINE_JUDGE
+	freopen("inputCF.txt","r",stdin);	freopen("inputCC.txt","r",stdin);	freopen("input.txt","r",stdin);
+	freopen("outputCF.txt","w",stdout);	freopen("outputCC.txt","w",stdout); freopen("output.txt","w",stdout);
+	#endif 								#endif 								#endif
+
+	#ifndef ONLINE_JUDGE				#ifndef ONLINE_JUDGE
+	freopen("inputHR.txt","r",stdin);	freopen("inputGFG.txt","r",stdin);
+    freopen("outputHR.txt","w",stdout); freopen("outputGFG.txt","w",stdout);
+	#endif 								#endif
+	*/
+	
+	#ifndef ONLINE_JUDGE
+	freopen("inputGFG.txt","r",stdin);
+	freopen("outputGFG.txt","w",stdout);
+	#endif
+	KILL HER NOW
+	LL t=1;
+	cin>>t;
+	go(t)
+	{
+		LL a,b;
+		cin>>a>>b;
+		cout<<(a%M + b%M)%M<<"\n";
+	}
+	return 0;
+}
