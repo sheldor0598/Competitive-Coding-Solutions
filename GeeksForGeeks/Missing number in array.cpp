cd03b52a44bc95072521e13925b351342c239848
shandilya <shubhamshandilya0598@gmail.com>
2020-06-26 16:04:54 +0530

    added 1 solution in GeeksForGeeks

---
+++ GeeksForGeeks/Missing number in array.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long int
+#define KILL ios_base::sync_with_stdio(false);
+#define HER cin.tie(NULL);
+#define goflessthan(i,a,b) for(i=a;i<b;i++)
+#define gofequalto(i,a,b) for(i=a;i<=b;i++)
+#define gofminus(i,a,b) for(i=a;i>=b;i--)
+#define go(t) while(t--)
+#define endl cout<<"\n"
+#define pb push_back
+#define pob pop_back
+#define mp make_pair
+
+int main()
+{
+	/*
+	#ifndef ONLINE_JUDGE
+	freopen("inputCF.txt","r",stdin);
+    freopen("outputCF.txt","w",stdout);
+	#endif
+	
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+
+	#ifndef ONLINE_JUDGE
+	freopen("inputCC.txt","r",stdin);
+    freopen("outputCC.txt","w",stdout);
+	#endif
+
+	#ifndef ONLINE_JUDGE
+	freopen("inputGFG.txt","r",stdin);
+    freopen("outputGFG.txt","w",stdout);
+	#endif
+	*/
+	#ifndef ONLINE_JUDGE
+	freopen("inputGFG.txt","r",stdin);
+    freopen("outputGFG.txt","w",stdout);
+	#endif
+	KILL HER
+	LL t;
+	cin>>t;
+	go(t)
+	{
+		LL n;
+		cin>>n;
+		LL A[n],B[n+1],i;
+		fill(B+1,B+n+1,0);
+        goflessthan(i,0,n-1)
+        {
+            cin>>A[i];
+            B[A[i]]++;
+        }
+        goflessthan(i,1,n+1)
+        {
+            if(B[i]==0)
+            {
+                cout<<i<<"\n";
+                break;
+            }
+        }
+	}
+	return 0;
+}
