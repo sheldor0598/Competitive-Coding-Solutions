b09c24d53826d815a7c3f2e52035aa77647791cd
shandilya <shubhamshandilya0598@gmail.com>
2020-06-29 07:49:19 +0530

    added codechef june lunchtime div.2 - CHFMOT18

---
+++ codechef/June Lunchtime 2020 Div.2/CHFMOT18.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long int
+#define KILL ios_base::sync_with_stdio(false);
+#define HER cin.tie(NULL);
+#define NOW cout.tie(NULL);
+#define fori(i,a,b) for(i=a;i<b;i++)
+#define forireverse(i,a,b) for(i=a;i>=b;i--)
+#define full(x) x.begin(),x.end()
+#define go(t) while(t--)
+#define endl cout<<"\n"
+#define pb push_back
+#define pob pop_back
+#define mp make_pair
+#define M 1000000007
+
+int main()
+{
+	/*
+	#ifndef ONLINE_JUDGE				#ifndef ONLINE_JUDGE				#ifndef ONLINE_JUDGE
+	freopen("inputCF.txt","r",stdin);	freopen("inputCC.txt","r",stdin);	freopen("input.txt","r",stdin);
+	freopen("outputCF.txt","w",stdout);	freopen("outputCC.txt","w",stdout); freopen("output.txt","w",stdout);
+	#endif 								#endif 								#endif
+
+	#ifndef ONLINE_JUDGE				#ifndef ONLINE_JUDGE
+	freopen("inputHR.txt","r",stdin);	freopen("inputGFG.txt","r",stdin);
+    freopen("outputHR.txt","w",stdout); freopen("outputGFG.txt","w",stdout);
+	#endif 								#endif
+	*/
+	
+	#ifndef ONLINE_JUDGE
+	freopen("inputCC.txt","r",stdin);
+	freopen("outputCC.txt","w",stdout);
+	#endif
+	KILL HER NOW
+	LL t=1;
+	cin>>t;
+	go(t)
+	{
+		LL n,s,p,q,k,l=0,m=0;
+		cin>>s>>n;
+		p=s/n;
+		q=s%n;
+		if(q>0 && q%2==0) p++;
+		else
+		{
+			k=q/2;
+			if(k>0)
+			{
+				l=(q/(2*k));
+				m=q%(2*k);
+			}
+			else p+=q;
+		}
+		cout<<p+l+m<<"\n";
+	}
+	return 0;
+}
