7c16d0d2f441cad9e0cf1ef53d6edfee73a728bc
shandilya <shubhamshandilya0598@gmail.com>
2020-07-01 09:14:39 +0530

    added 1 solution in codeforces

---
+++ codeforces/Tokitsukaze and Enhancement.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long int
+#define KILL ios_base::sync_with_stdio(false);
+#define HER cin.tie(NULL);
+#define goflessthan(i,a,b) for(i=a;i<b;i++)
+#define gofequalto(i,a,b) for(i=a;i<=b;i++)
+#define gofminus(i,a,b) for(i=a;i>=b;i--)
+#define go(t) while(t--)
+#define endl cout<<"\n"
+#define pb push_back
+#define mp make_pair
+
+int main()
+{
+	/*
+	#ifndef ONLINE_JUDGE
+	freopen("inputCF.txt","r",stdin);
+    freopen("outputCF.txt","w",stdout);
+	#endif
+	
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+
+	#ifndef ONLINE_JUDGE
+	freopen("inputCC.txt","r",stdin);
+    freopen("outputCC.txt","w",stdout);
+	#endif
+
+	#ifndef ONLINE_JUDGE
+	freopen("inputGFG.txt","r",stdin);
+    freopen("outputGFG.txt","w",stdout);
+	#endif
+	*/
+	#ifndef ONLINE_JUDGE
+	freopen("inputCF.txt","r",stdin);
+    freopen("outputCF.txt","w",stdout);
+	#endif
+	KILL HER
+	LL n,p=0;
+	cin>>n;
+	if(n%4==3) p+=2;
+	else if(n%4==2) p+=1;
+	else if(n%4==0) p+=1;
+	n+=p;
+	if(n%4==1) cout<<p<<" "<<'A';
+	else if(n%4==2) cout<<p<<" "<<'C';
+	else if(n%4==3) cout<<p<<" "<<'B';
+	return 0;
+}
