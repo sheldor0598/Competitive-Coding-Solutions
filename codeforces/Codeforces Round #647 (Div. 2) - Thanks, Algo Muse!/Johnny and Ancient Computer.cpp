f7618cb6a135abc6be7c2425247c9a6dbd9cfcdb
shandilya <shubhamshandilya0598@gmail.com>
2020-07-03 17:53:26 +0530

    added folder Codeforces Round #647 (Div. 2) - Thanks, Algo Muse! and files moved

---
+++ codeforces/Codeforces Round #647 (Div. 2) - Thanks, Algo Muse!/Johnny and Ancient Computer.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long int
+#define KILL ios_base::sync_with_stdio(false);
+#define HER cin.tie(NULL);
+#define NOW cout.tie(NULL);
+#define fori(i,a,b) for(i=a;i<b;i++)
+#define forireverse(i,a,b) for(i=a;i>=b;i--)
+#define full(x) x.begin(),x.end()
+#define go(t) while(t--)
+#define endl cout<<"\n"
+#define pb push_back
+#define pob pop_back
+#define mp make_pair
+#define M 1000000007
+
+int main()
+{
+	/*
+	#ifndef ONLINE_JUDGE
+	freopen("inputCF.txt","r",stdin);
+    freopen("outputCF.txt","w",stdout);
+	#endif
+	
+	#ifndef ONLINE_JUDGE
+	freopen("inputHR.txt","r",stdin);
+    freopen("outputHR.txt","w",stdout);
+	#endif
+
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+
+	#ifndef ONLINE_JUDGE
+	freopen("inputCC.txt","r",stdin);
+    freopen("outputCC.txt","w",stdout);
+	#endif
+
+	#ifndef ONLINE_JUDGE
+	freopen("inputGFG.txt","r",stdin);
+    freopen("outputGFG.txt","w",stdout);
+	#endif
+	*/
+	#ifndef ONLINE_JUDGE
+	freopen("inputCF.txt","r",stdin);
+    freopen("outputCF.txt","w",stdout);
+	#endif
+	KILL HER NOW
+	LL t;
+	cin>>t;
+	go(t)
+	{
+		LL a,b,cnt=0;
+		cin>>a>>b;
+		LL maxm=max(a,b);
+		LL minm=min(a,b);
+		a=minm;
+		b=maxm;
+		while(a<b)
+		{
+			if(a*8<=b) a*=8;
+			else if(a*4<=b) a*=4;
+			else a*=2;
+			cnt++;
+		}
+		if(a==b) cout<<cnt<<"\n";
+		else cout<<-1<<"\n";
+        
+	}
+	return 0;
+}
