1a89b0f8888f815d836c9d9fb5882e5c95a65731
shandilya <shubhamshandilya0598@gmail.com>
2020-07-04 19:54:12 +0530

    added 1 solution in codechef

---
+++ codechef/EXAMCHT.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long int
+#define KILL ios_base::sync_with_stdio(false);
+#define HER cin.tie(NULL);
+#define goflessthan(i,a,b) for(i=a;i<b;i++)
+#define gofequalto(i,a,b) for(i=a;i<=b;i++)
+#define gofminus(i,a,b) for(i=a;i>=b;i--)
+#define go(t) while(t--)
+#define endl cout<<"\n"
+#define pb push_back
+#define mp make_pair
+
+int main()
+{
+	/*
+	#ifndef ONLINE_JUDGE
+	freopen("inputCF.txt","r",stdin);
+    freopen("outputCF.txt","w",stdout);
+	#endif
+	
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+
+	#ifndef ONLINE_JUDGE
+	freopen("inputCC.txt","r",stdin);
+    freopen("outputCC.txt","w",stdout);
+	#endif
+
+	#ifndef ONLINE_JUDGE
+	freopen("inputGFG.txt","r",stdin);
+    freopen("outputGFG.txt","w",stdout);
+	#endif
+	*/
+	#ifndef ONLINE_JUDGE
+	freopen("inputCC.txt","r",stdin);
+    freopen("outputCC.txt","w",stdout);
+	#endif
+	KILL HER
+	LL t;
+	cin>>t;
+	go(t)
+	{
+		LL a,b,p=1,m,i,w,n;
+		vector<LL> A;
+        cin>>a>>b;
+        if(a==b)
+    	{
+    		cout<<-1<<"\n";
+    		continue;
+    	}
+        a--;b--;
+        m=max(a,b);
+        n=min(a,b);
+        w=m-n;
+        for(i=1;i<=sqrt(w);i++)
+        {
+        	if(w%i==0)
+        	{
+        		if(w/i == i) A.pb(i);
+        		else { A.pb(i);A.pb(w/i); }
+        	}
+        }
+        cout<<A.size()<<"\n";
+	}
+	return 0;
+}
