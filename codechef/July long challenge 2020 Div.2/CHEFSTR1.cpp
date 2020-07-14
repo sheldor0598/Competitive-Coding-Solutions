01de8eafc4d56c1bd9aa11a3a0fbee2a35b3ea0e
shandilya <shubhamshandilya0598@gmail.com>
2020-07-14 09:39:08 +0530

    added A.CHEFSTR1.cpp solution in codechef july long challenge 2020 div 2

---
+++ codechef/July long challenge 2020 Div.2/CHEFSTR1.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long int
+#define KILL ios_base::sync_with_stdio(false);
+#define HER cin.tie(NULL);
+#define NOW cout.tie(NULL);
+#define fori(i,a,b) for(i=a;i<b;i++)
+#define forireverse(i,a,b) for(i=a;i>=b;i--)
+#define full(x) x.begin(),x.end()
+#define go(t) while(t--)
+#define endl cout<<"\n"
+#define pb push_back
+#define pob pop_back
+#define mp make_pair
+#define M 1000000007
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+	freopen("inputCC.txt","r",stdin);
+	freopen("outputCC.txt","w",stdout);
+	#endif
+	KILL HER NOW
+	LL t=1;
+	cin>>t;
+	go(t)
+	{
+		LL n;
+		cin>>n;
+		LL A[n],i,sum=0;
+        fori(i,0,n) cin>>A[i];
+        fori(i,1,n) sum+=abs(A[i]-A[i-1])-1;
+        cout<<sum<<"\n";
+	}
+	return 0;
+}
