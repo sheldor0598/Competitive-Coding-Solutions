ab563f46bf16ef113856c6b968c91f039917e648
shandilya <shubhamshandilya0598@gmail.com>
2020-07-15 20:57:29 +0530

    added 1 solution in codechef

---
+++ codechef/CLIPLX.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long int
+#define KILL ios_base::sync_with_stdio(false);
+#define HER cin.tie(NULL);
+#define NOW cout.tie(NULL);
+#define fori(i,a,b) for(i=a;i<b;i++)
+#define forireverse(i,a,b) for(i=a;i>=b;i--)
+#define full(x) x.begin(),x.end()
+#define go(t) while(t--)
+#define endl cout<<"\n"
+#define pb push_back
+#define pob pop_back
+#define mp make_pair
+#define M 1000000007
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+	freopen("inputCF.txt","r",stdin);
+	freopen("outputCF.txt","w",stdout);
+	#endif
+	KILL HER NOW
+	LL t=1;
+	cin>>t;
+	go(t)
+	{
+		LL x,y,p=0;
+		cin>>x>>y;
+		if(x>=y) cout<<x-y<<"\n";
+		else cout<<0<<"\n";
+        
+	}
+	return 0;
+}
