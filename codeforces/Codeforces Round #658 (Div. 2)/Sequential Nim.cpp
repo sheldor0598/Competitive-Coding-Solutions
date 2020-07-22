f1c9daceb2a37ad692163fed8d2874308cd5f9ba
shandilya <shubhamshandilya0598@gmail.com>
2020-07-22 11:11:46 +0530

    added 1st solution of Codeforces Round #658 (Div. 2)

---
+++ codeforces/Codeforces Round #658 (Div. 2)/Sequential Nim.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long int
+#define KILL ios_base::sync_with_stdio(false);
+#define HER cin.tie(NULL);
+#define NOW cout.tie(NULL);
+#define fori(i,a,b) for(i=a;i<b;i++)
+#define forireverse(i,a,b) for(i=a;i>=b;i--)
+#define sortV(x) sort(x.begin(),x.end())
+#define sortVgr(x) sort(x.begin(),x.end(),greater<LL>())
+#define go(t) while(t--)
+#define endl cout<<"\n"
+#define pb push_back
+#define pob pop_back
+#define mp make_pair
+#define M 1000000007
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+	freopen("inputCF.txt","r",stdin);
+	freopen("outputCF.txt","w",stdout);
+	#endif
+	KILL HER NOW
+	LL t=1;
+	cin>>t;
+	go(t)
+	{
+		LL n,f=1;
+		cin>>n;
+		LL A[n],i;
+        fori(i,0,n) cin>>A[i];
+        fori(i,0,n)
+        {
+        	if(A[i]==1) f^=1;
+        	else {f^=1;break;}
+        }
+        if(f==0) cout<<"First\n";
+        else cout<<"Second\n";
+	}
+	return 0;
+}
