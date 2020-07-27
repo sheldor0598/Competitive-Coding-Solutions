e9e6b01a339ca157fb5322034623341fddd353e7
shandilya <shubhamshandilya0598@gmail.com>
2020-07-27 18:38:05 +0530

    added 1 solution in codeforces

---
+++ codeforces/Buy a Shovel.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long int
+#define KILL ios_base::sync_with_stdio(false);
+#define HER cin.tie(NULL);
+#define NOW cout.tie(NULL);
+#define fori(i,a,b) for(i=a;i<b;i++)
+#define forireverse(i,a,b) for(i=a;i>=b;i--)
+#define sortV(x) sort(x.begin(),x.end())
+#define sortVgr(x) sort(x.begin(),x.end(),greater<LL>())
+#define go(t) while(t--)
+#define endl cout<<"\n"
+#define pb push_back
+#define pob pop_back
+#define mp make_pair
+#define M 1000000007
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+	freopen("inputCF.txt","r",stdin);
+	freopen("outputCF.txt","w",stdout);
+	#endif
+	KILL HER NOW
+	LL t=1;
+	//cin>>t;
+	go(t)
+	{
+		LL n,k,cnt=1,i=1;
+		cin>>n>>k;
+		LL sum=n;
+		while(i>0)
+		{		
+			if(sum%10==0 || sum%10==k) break;
+			sum+=n;
+			cnt++;
+		}
+        cout<<cnt;
+	}
+	return 0;
+}
