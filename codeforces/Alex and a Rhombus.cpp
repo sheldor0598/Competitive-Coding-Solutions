e063f26707971349280468a945978f75a982c695
shandilya <shubhamshandilya0598@gmail.com>
2020-07-29 08:33:49 +0530

    added 1 solution in codeforces

---
+++ codeforces/Alex and a Rhombus.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long int
+#define KILL ios_base::sync_with_stdio(false);
+#define HER cin.tie(NULL);
+#define NOW cout.tie(NULL);
+#define fori(i,a,b) for(i=a;i<b;i++)
+#define forireverse(i,a,b) for(i=a;i>=b;i--)
+#define sortV(x) sort(x.begin(),x.end())
+#define sortVgr(x) sort(x.begin(),x.end(),greater<LL>())
+#define go(t) while(t--)
+#define endl cout<<"\n"
+#define pb push_back
+#define pob pop_back
+#define mp make_pair
+#define M 1000000007
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+	freopen("inputCF.txt","r",stdin);
+	freopen("outputCF.txt","w",stdout);
+	#endif
+	KILL HER NOW
+	LL t=1;
+	//cin>>t;
+	go(t)
+	{
+		LL n;
+		cin>>n;
+		LL A[101],i;
+		A[0]=1;
+        fori(i,1,101) A[i]=A[i-1] + 4*i;
+        cout<<A[n-1];
+	}
+	return 0;
+}
