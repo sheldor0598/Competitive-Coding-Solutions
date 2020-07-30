aa56f8045071519998f68fea45a97dd7e5a5d8b5
shandilya <shubhamshandilya0598@gmail.com>
2020-07-30 09:11:39 +0530

    added A.LCM Problem solution in Educational Codeforces Round 92 (Rated for Div. 2)

---
+++ codeforces/Educational Codeforces Round 92 (Rated for Div. 2)/LCM Problem.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long int
+#define KILL ios_base::sync_with_stdio(false);
+#define HER cin.tie(NULL);
+#define NOW cout.tie(NULL);
+#define fori(i,a,b) for(i=a;i<b;i++)
+#define forireverse(i,a,b) for(i=a;i>=b;i--)
+#define sortV(x) sort(x.begin(),x.end())
+#define sortVgr(x) sort(x.begin(),x.end(),greater<LL>())
+#define go(t) while(t--)
+#define endl cout<<"\n"
+#define pb push_back
+#define pob pop_back
+#define mp make_pair
+#define M 1000000007
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+	freopen("inputCF.txt","r",stdin);
+	freopen("outputCF.txt","w",stdout);
+	#endif
+	KILL HER NOW
+	LL t=1;
+	cin>>t;
+	go(t)
+	{
+		LL a,b;
+		cin>>a>>b;
+		if(2*a>b) cout<<-1<<" "<<-1<<"\n";
+		else cout<<a<<" "<<2*a<<"\n";
+	}
+	return 0;
+}
