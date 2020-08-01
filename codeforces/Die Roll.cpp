e40454b60ddf9340479bca0903886ef8cd4663d7
shandilya <shubhamshandilya0598@gmail.com>
2020-08-01 08:33:52 +0530

    added 1 solution in codeforces

---
+++ codeforces/Die Roll.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long int
+#define KILL ios_base::sync_with_stdio(false);
+#define HER cin.tie(NULL);
+#define NOW cout.tie(NULL);
+#define fori(i,a,b) for(i=a;i<b;i++)
+#define forireverse(i,a,b) for(i=a;i>=b;i--)
+#define sortV(x) sort(x.begin(),x.end())
+#define sortVgr(x) sort(x.begin(),x.end(),greater<LL>())
+#define go(t) while(t--)
+#define endl cout<<"\n"
+#define pb push_back
+#define pob pop_back
+#define mp make_pair
+#define M 1000000007
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+	freopen("inputCF.txt","r",stdin);
+	freopen("outputCF.txt","w",stdout);
+	#endif
+	KILL HER NOW
+	LL t=1;
+	//cin>>t;
+	go(t)
+	{
+		LL a,b,c,g,_6=6;
+		cin>>a>>b;
+		c=_6 - max(a,b) + 1;
+		g=__gcd(c,_6);
+		if(c%g==0 && 6%g==0) cout<<c/g<<'/'<<6/g;
+		else cout<<c<<'/'<<6;
+	}
+	return 0;
+}
