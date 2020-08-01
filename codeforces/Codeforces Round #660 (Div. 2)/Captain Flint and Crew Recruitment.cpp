22142eaf15d769a14de3f70082001276b8c9597d
shandilya <shubhamshandilya0598@gmail.com>
2020-08-01 08:34:32 +0530

    added Codeforces Round #660 (Div. 2)

---
+++ codeforces/Codeforces Round #660 (Div. 2)/Captain Flint and Crew Recruitment.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long int
+#define KILL ios_base::sync_with_stdio(false);
+#define HER cin.tie(NULL);
+#define NOW cout.tie(NULL);
+#define fori(i,a,b) for(i=a;i<b;i++)
+#define forireverse(i,a,b) for(i=a;i>=b;i--)
+#define sortV(x) sort(x.begin(),x.end())
+#define sortVgr(x) sort(x.begin(),x.end(),greater<LL>())
+#define go(t) while(t--)
+#define endl cout<<"\n"
+#define pb push_back
+#define pob pop_back
+#define pf push_front
+#define pof pop_front
+#define mp make_pair
+#define M 1000000007
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+	freopen("inputCF.txt","r",stdin);
+	freopen("outputCF.txt","w",stdout);
+	#endif
+	KILL HER NOW
+	LL t=1;
+	cin>>t;
+	go(t)
+	{
+		LL n;
+		cin>>n;
+		if(n<=30) cout<<"NO\n";
+		else
+		{
+			cout<<"YES\n";
+			if(n-30==6 || n-30==10 || n-30==14) cout<<6<<" "<<10<<" "<<15<<" "<<n-31<<"\n";
+			else cout<<6<<" "<<10<<" "<<14<<" "<<n-30<<"\n";
+		}
+	}
+	return 0;
+}
