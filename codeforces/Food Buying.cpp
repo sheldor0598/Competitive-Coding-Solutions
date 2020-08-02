07bf4f377c98ba7e4fba3d5bef8498c1a732e7be
shandilya <shubhamshandilya0598@gmail.com>
2020-08-02 11:34:27 +0530

    added 1 solution in codeforces

---
+++ codeforces/Food Buying.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long int
+#define KILL ios_base::sync_with_stdio(false);
+#define HER cin.tie(NULL);
+#define NOW cout.tie(NULL);
+#define fori(i,a,b) for(i=a;i<b;i++)
+#define forireverse(i,a,b) for(i=a;i>=b;i--)
+#define sortV(x) sort(x.begin(),x.end())
+#define sortVgr(x) sort(x.begin(),x.end(),greater<LL>())
+#define go(t) while(t--)
+#define endl cout<<"\n"
+#define pb push_back
+#define pob pop_back
+#define pf push_front
+#define pof pop_front
+#define mp make_pair
+#define M 1000000007
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+	freopen("inputCF.txt","r",stdin);
+	freopen("outputCF.txt","w",stdout);
+	#endif
+	KILL HER NOW
+	LL t=1;
+	cin>>t;
+	go(t)
+	{
+		LL n,p;
+		cin>>n;
+		LL sum=n,cnt=0;
+		if(n<10) {cout<<n<<"\n";continue;}
+		while(n>=10)
+		{
+			sum+=(n/10);
+			p=n%10;
+			n/=10;
+			n+=p;
+		}
+		cout<<sum<<"\n";
+	}
+	return 0;
+}
