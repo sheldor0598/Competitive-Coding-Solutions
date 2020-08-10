b173bb83c85fef3ce4728e9f2afb11a7d03490f2
shandilya <shubhamshandilya0598@gmail.com>
2020-08-10 19:11:43 +0530

    added A solution in Codeforces Round #663 (Div. 2)

---
+++ codeforces/Codeforces Round #663 (Div. 2)/Fix You.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long int
+#define KILL ios_base::sync_with_stdio(false);
+#define HER cin.tie(NULL);
+#define NOW cout.tie(NULL);
+#define fori(i,a,b) for(i=a;i<b;i++)
+#define forireverse(i,a,b) for(i=a;i>=b;i--)
+#define full(x) x.begin(),x.end()
+#define sortV(x) sort(full(x))
+#define sortVgr(x) sort(full(x),greater<LL>()) // x -> vector
+#define reverse(x) reverse(full(x)) // x -> string
+#define go(t) while(t--)
+#define endl cout<<"\n"
+#define pb push_back
+#define pob pop_back
+#define pf push_front
+#define pof pop_front
+#define mp make_pair
+#define M 1e9+7
+
+LL gcd(LL a, LL b)
+{
+	if(a==0) return b;
+	else return gcd(b%a,a);
+}
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+	freopen("inputCF.txt","r",stdin);
+	freopen("outputCF.txt","w",stdout);
+	#endif
+	KILL HER NOW
+	LL t=1;
+	cin>>t;
+	go(t)
+	{
+		LL n,m,cnt=0;
+		cin>>n>>m;
+		LL i,j;
+		char A[n][m];
+        fori(i,0,n) fori(j,0,m) cin>>A[i][j];
+        fori(i,0,n)
+        	fori(j,0,m)
+        	{
+        		if(j==m-1 && A[i][j]=='R') cnt++;
+        		if(i==n-1 && A[i][j]=='D') cnt++;
+        	}
+    	cout<<cnt<<"\n";
+	}
+	return 0;
+}
