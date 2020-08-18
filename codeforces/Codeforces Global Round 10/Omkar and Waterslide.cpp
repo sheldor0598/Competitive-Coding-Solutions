2675c5b51a0844a19de42e6f21a3b22d59a608ec
shandilya <shubhamshandilya0598@gmail.com>
2020-08-18 08:26:20 +0530

    added C.Omkar and Waterslide in Codeforces Global Round 10

---
+++ codeforces/Codeforces Global Round 10/Omkar and Waterslide.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long int
+#define FAST ios_base::sync_with_stdio(false);
+#define INPT cin.tie(NULL);
+#define OUTPT cout.tie(NULL);
+#define fori(i,a,b) for(i=a;i<b;i++)
+#define forireverse(i,a,b) for(i=a;i>=b;i--)
+#define full(x) x.begin(),x.end()
+#define sortV(x) sort(full(x))
+#define sortVgr(x) sort(full(x),greater<LL>()) // x -> vector
+#define reverse(x) reverse(full(x)) // x -> string
+#define go(t) while(t--)
+#define endl cout<<"\n"
+#define pb push_back
+#define pob pop_back
+#define pf push_front
+#define pof pop_front
+#define mp make_pair
+#define M 1e9+7
+
+LL gcd(LL a, LL b)
+{
+    if(a==0) return b;
+    else return gcd(b%a,a);
+}
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("inputCF.txt","r",stdin);
+    freopen("outputCF.txt","w",stdout);
+    #endif
+    FAST INPT OUTPT
+    LL t=1;
+    cin>>t;
+    go(t)
+    {
+        LL n,cnt=0;
+        cin>>n;
+        LL A[n],i;
+        fori(i,0,n) cin>>A[i];
+        fori(i,0,n-1) if(A[i]-A[i+1]>=0) cnt+=A[i]-A[i+1];
+        cout<<cnt<<"\n";
+    }
+    return 0;
+}
