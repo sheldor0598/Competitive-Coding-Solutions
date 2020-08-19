684a4d19da67bd825c77cbcb8d47ca40b988cf71
shandilya <shubhamshandilya0598@gmail.com>
2020-08-19 18:32:35 +0530

    added B.Captain Flint and a Long Voyage in Codeforces Round #660 (Div. 2)

---
+++ codeforces/Codeforces Round #660 (Div. 2)/Captain Flint and a Long Voyage.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long int
+#define KILL ios_base::sync_with_stdio(false);
+#define HER cin.tie(NULL);
+#define NOW cout.tie(NULL);
+#define fori(i,a,b) for(i=a;i<b;i++)
+#define forireverse(i,a,b) for(i=a;i>=b;i--)
+#define sortV(x) sort(x.begin(),x.end())
+#define sortVgr(x) sort(x.begin(),x.end(),greater<LL>())
+#define go(t) while(t--)
+#define endl cout<<"\n"
+#define pb push_back
+#define pob pop_back
+#define pf push_front
+#define pof pop_front
+#define mp make_pair
+#define M 1000000007
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+	freopen("inputCF.txt","r",stdin);
+	freopen("outputCF.txt","w",stdout);
+	#endif
+	KILL HER NOW
+	LL t=1;
+	cin>>t;
+	go(t)
+	{
+		LL n,k,p,d;
+		cin>>n;
+		p=(n+3)/4;
+		k=n-p;
+		while(k>0) {cout<<9; k--;}
+		while(p>0) {cout<<8; p--;}
+		endl;
+	}
+	return 0;
+}
