7ff86bb47eb9799a2264ae7fbc7cbeb4a651b144
shandilya <shubhamshandilya0598@gmail.com>
2020-08-19 18:33:43 +0530

    added CHEFWARS in August long challenge 2020 Div.2

---
+++ codechef/August long challenge 2020 Div.2/CHEFWARS.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long int
+#define FAST ios_base::sync_with_stdio(false);
+#define INPT cin.tie(NULL);
+#define OUTPT cout.tie(NULL);
+#define fori(i,a,b) for(i=a;i<b;i++)
+#define forireverse(i,a,b) for(i=a;i>=b;i--)
+#define full(x) x.begin(),x.end()
+#define sortV(x) sort(full(x))
+#define sortVgr(x) sort(full(x),greater<LL>()) // x -> vector
+#define reverse(x) reverse(full(x)) // x -> string
+#define go(t) while(t--)
+#define endl cout<<"\n"
+#define pb push_back
+#define pob pop_back
+#define pf push_front
+#define pof pop_front
+#define mp make_pair
+#define M 1e9+7
+
+LL gcd(LL a, LL b)
+{
+	if(a==0) return b;
+	else return gcd(b%a,a);
+}
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+	freopen("inputCC.txt","r",stdin);
+	freopen("outputCC.txt","w",stdout);
+	#endif
+	FAST INPT OUTPT
+	LL t=1;
+	cin>>t;
+	go(t)
+	{
+		LL a,b;
+		cin>>a>>b;
+		while(a>0 && b>0)
+		{
+			a-=b;
+			b/=2;
+		}
+		if(a>0) cout<<0;
+		else cout<<1; endl;
+	}
+	return 0;
+}
