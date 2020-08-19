d52e908837f44470c4f0aad56655d1fc4b9296cc
shandilya <shubhamshandilya0598@gmail.com>
2020-08-19 18:34:47 +0530

    added CRDGAME3 in August long challenge 2020 Div.2

---
+++ codechef/August long challenge 2020 Div.2/CRDGAME3.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long int
+#define FAST ios_base::sync_with_stdio(false);
+#define INPT cin.tie(NULL);
+#define OUTPT cout.tie(NULL);
+#define fori(i,a,b) for(i=a;i<b;i++)
+#define forireverse(i,a,b) for(i=a;i>=b;i--)
+#define full(x) x.begin(),x.end()
+#define sortV(x) sort(full(x))
+#define sortVgr(x) sort(full(x),greater<LL>()) // x -> vector
+#define reverse(x) reverse(full(x)) // x -> string
+#define go(t) while(t--)
+#define endl cout<<"\n"
+#define pb push_back
+#define pob pop_back
+#define pf push_front
+#define pof pop_front
+#define mp make_pair
+#define M 1e9+7
+
+LL gcd(LL a, LL b)
+{
+	if(a==0) return b;
+	else return gcd(b%a,a);
+}
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+	freopen("inputCC.txt","r",stdin);
+	freopen("outputCC.txt","w",stdout);
+	#endif
+	FAST INPT OUTPT
+	LL t=1;
+	cin>>t;
+	go(t)
+	{
+		LL n,m,i=1,p=1,cntc=1,cntr=1;
+		cin>>n>>m;
+		if(n>9)
+		{
+			cntc=n/9; 
+			if(n%9!=0) cntc++; 
+		}
+		if(m>9) 
+		{
+			cntr=m/9;
+			if(m%9!=0) cntr++;
+		}
+		if(cntc<cntr) cout<<0<<" "<<cntc;
+		else cout<<1<<" "<<cntr; endl;
+	}
+	return 0;
+}
