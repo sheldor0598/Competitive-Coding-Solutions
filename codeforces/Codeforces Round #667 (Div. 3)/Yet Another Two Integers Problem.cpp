1030935a5d659de9bcfceb1949058c74b6fe6bdf
shandilya <shubhamshandilya0598@gmail.com>
2020-09-04 22:27:22 +0530

    added Yet Another Two Integers Problem in Codeforces Round #667 (Div. 3)

---
+++ codeforces/Codeforces Round #667 (Div. 3)/Yet Another Two Integers Problem.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long int
+#define FAST ios_base::sync_with_stdio(false);
+#define INPT cin.tie(NULL);
+#define OUTPT cout.tie(NULL);
+#define fori(i,a,b) for(i=a;i<b;i++)
+#define forireverse(i,a,b) for(i=a;i>=b;i--)
+#define full(x) x.begin(),x.end()
+#define sortV(x) sort(full(x))
+#define sortVgr(x) sort(full(x),greater<LL>()) // x -> vector
+#define reverse(x) reverse(full(x)) // x -> string
+#define go(t) while(t--)
+#define endl cout<<"\n"
+#define pb push_back
+#define pob pop_back
+#define pf push_front
+#define pof pop_front
+#define mp make_pair
+#define M 1e9+7
+
+LL gcd(LL a, LL b)
+{
+	if(a==0) return b;
+	else return gcd(b%a,a);
+}
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+	freopen("inputCF.txt","r",stdin);
+	freopen("outputCF.txt","w",stdout);
+	#endif
+	FAST INPT OUTPT
+	LL t=1;
+	cin>>t;
+	go(t)
+	{
+		LL a,b,i,p,diff,cnt=0,k=10;
+		cin>>a>>b;
+		if(a==b) {cout<<0<<"\n";continue;}
+		p=abs(a-b);
+		cnt+=p/10;
+		k=p%10;
+		if(k>0) cnt++;
+		cout<<cnt<<"\n";
+	}
+	return 0;
+}
