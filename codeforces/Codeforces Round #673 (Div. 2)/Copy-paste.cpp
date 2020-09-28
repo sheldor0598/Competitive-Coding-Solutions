a281eaa04aef5d9412691b331ec39018b46b3347
shandilya <shubhamshandilya0598@gmail.com>
2020-09-28 13:33:22 +0530

    added A.Copy-paste in Codeforces Round #673 (Div. 2)

---
+++ codeforces/Codeforces Round #673 (Div. 2)/Copy-paste.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long int
+#define FAST ios_base::sync_with_stdio(false);
+#define INPT cin.tie(NULL);
+#define OUTPT cout.tie(NULL);
+#define fori(i,a,b) for(i=a;i<b;i++)
+#define forireverse(i,a,b) for(i=a;i>=b;i--)
+#define full(x) x.begin(),x.end()
+#define sortV(x) sort(full(x))
+#define sortVgr(x) sort(full(x),greater<LL>()) // x -> vector
+#define reverse(x) reverse(full(x)) // x -> string
+#define go(t) while(t--)
+#define endl cout<<"\n"
+#define pb push_back
+#define pob pop_back
+#define pf push_front
+#define pof pop_front
+#define mp make_pair
+#define M 1e9+7
+
+LL gcd(LL a, LL b)
+{
+	if(a==0) return b;
+	else return gcd(b%a,a);
+}
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+	freopen("inputCF.txt","r",stdin);
+	freopen("outputCF.txt","w",stdout);
+	#endif
+	FAST INPT OUTPT
+	LL t=1;
+	cin>>t;
+	go(t)
+	{
+		LL n,k,p,cnt=0;
+		cin>>n>>k;
+		LL A[n],i;
+        fori(i,0,n) cin>>A[i];
+        sort(A,A+n);
+        p=A[0];
+        fori(i,1,n)
+        {
+        	while(A[i]+p <=k) 
+        	{
+        		cnt++;
+        		A[i]+=p;
+        	}
+        }
+        cout<<cnt<<"\n";
+	}
+	return 0;
+}
