a4380f03b9000f452731796fa721c6d4b7cc1519
shandilya <shubhamshandilya0598@gmail.com>
2020-09-29 08:52:46 +0530

    added B.Symmetric Matrix in Codeforces Round #674 (Div. 3)

---
+++ codeforces/Codeforces Round #674 (Div. 3)/Symmetric Matrix.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long int
+#define FAST ios_base::sync_with_stdio(false);
+#define INPT cin.tie(NULL);
+#define OUTPT cout.tie(NULL);
+#define fori(i,a,b) for(i=a;i<b;i++)
+#define forireverse(i,a,b) for(i=a;i>=b;i--)
+#define full(x) x.begin(),x.end()
+#define sortV(x) sort(full(x))
+#define sortVgr(x) sort(full(x),greater<LL>()) // x -> vector
+#define reverse(x) reverse(full(x)) // x -> string
+#define go(t) while(t--)
+#define endl cout<<"\n"
+#define pb push_back
+#define pob pop_back
+#define pf push_front
+#define pof pop_front
+#define mp make_pair
+#define M 1e9+7
+
+LL gcd(LL a, LL b)
+{
+	if(a==0) return b;
+	else return gcd(b%a,a);
+}
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+	freopen("inputCF.txt","r",stdin);
+	freopen("outputCF.txt","w",stdout);
+	#endif
+	FAST INPT OUTPT
+	LL t=1;
+	cin>>t;
+	go(t)
+	{
+		LL n,m,a,b,c,d;
+		cin>>n>>m;
+		LL A[2][2],i,j,cnt=0,k;
+        fori(k,0,n)
+        {
+        	cin>>a>>b;
+        	cin>>c>>d;
+        	if(b==c) cnt++;
+        }
+        //cout<<cnt<<" ";
+        if(m%2==0 && cnt>0) cout<<"YES\n";
+        else cout<<"NO\n";   
+	}
+	return 0;
+}
