36320af4702d4215bd754322348ef7d1eb3c432e
shandilya <shubhamshandilya0598@gmail.com>
2020-09-29 09:28:36 +0530

    added D.Non-zero Segments in Codeforces Round #674 (Div. 3)

---
+++ codeforces/Codeforces Round #674 (Div. 3)/Non-zero Segments.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long int
+#define FAST ios_base::sync_with_stdio(false);
+#define INPT cin.tie(NULL);
+#define OUTPT cout.tie(NULL);
+#define fori(i,a,b) for(i=a;i<b;i++)
+#define forireverse(i,a,b) for(i=a;i>=b;i--)
+#define full(x) x.begin(),x.end()
+#define sortV(x) sort(full(x))
+#define sortVgr(x) sort(full(x),greater<LL>()) // x -> vector
+#define reverse(x) reverse(full(x)) // x -> string
+#define go(t) while(t--)
+#define endl cout<<"\n"
+#define pb push_back
+#define pob pop_back
+#define pf push_front
+#define pof pop_front
+#define mp make_pair
+#define M 1e9+7
+
+LL gcd(LL a, LL b)
+{
+	if(a==0) return b;
+	else return gcd(b%a,a);
+}
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+	freopen("inputCF.txt","r",stdin);
+	freopen("outputCF.txt","w",stdout);
+	#endif
+	FAST INPT OUTPT
+	LL t=1;
+	//cin>>t;
+	go(t)
+	{
+		LL n,a,cnt=0,sum=0;
+		cin>>n;
+		set<LL> s;
+		s.insert(0);
+		while(n--)
+		{
+			cin>>a;
+			sum+=a;
+			if(s.count(sum))
+			{
+				cnt++;s.clear();
+				s.insert(0);s.insert(a);
+				sum=a;
+			}
+			else s.insert(sum);
+		}
+        cout<<cnt;
+	}
+	return 0;
+}
