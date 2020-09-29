8ca4f35d0aafc552083d723bb1162c032ef538ed
shandilya <shubhamshandilya0598@gmail.com>
2020-09-29 09:29:14 +0530

    added E.Rock, Paper, Scissors in Codeforces Round #674 (Div. 3)

---
+++ codeforces/Codeforces Round #674 (Div. 3)/Rock, Paper, Scissors.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long int
+#define FAST ios_base::sync_with_stdio(false);
+#define INPT cin.tie(NULL);
+#define OUTPT cout.tie(NULL);
+#define fori(i,a,b) for(i=a;i<b;i++)
+#define forireverse(i,a,b) for(i=a;i>=b;i--)
+#define full(x) x.begin(),x.end()
+#define sortV(x) sort(full(x))
+#define sortVgr(x) sort(full(x),greater<LL>()) // x -> vector
+#define reverse(x) reverse(full(x)) // x -> string
+#define go(t) while(t--)
+#define endl cout<<"\n"
+#define pb push_back
+#define pob pop_back
+#define pf push_front
+#define pof pop_front
+#define mp make_pair
+#define M 1e9+7
+
+LL gcd(LL a, LL b)
+{
+	if(a==0) return b;
+	else return gcd(b%a,a);
+}
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+	freopen("inputCF.txt","r",stdin);
+	freopen("outputCF.txt","w",stdout);
+	#endif
+	FAST INPT OUTPT
+	LL t=1;
+	//cin>>t;
+	go(t)
+	{
+		LL n,ra,rb,sa,sb,pa,pb,minm,maxm,x=0;
+		cin>>n;
+		cin>>ra>>sa>>pa;
+		cin>>rb>>sb>>pb;
+        maxm=min(ra,sb) + min(sa,pb) + min(pa,rb);
+        minm=max({x, ra+sb-n , sa+pb-n , pa+rb-n});
+        cout<<minm<<" "<<maxm;
+	}
+	return 0;
+}
