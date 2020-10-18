9704a5375426de63fd246be593d08a93a0cfd2a3
shandilya <shubhamshandilya0598@gmail.com>
2020-10-18 21:04:22 +0530

    added A.XORwice in Codeforces Round #676 (Div. 2)

---
+++ codeforces/Codeforces Round #676 (Div. 2)/XORwice.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long int
+#define FAST ios_base::sync_with_stdio(false);
+#define INPT cin.tie(NULL);
+#define OUTPT cout.tie(NULL);
+#define fori(i,a,b) for(i=a;i<b;i++)
+#define forireverse(i,a,b) for(i=a;i>=b;i--)
+#define full(x) x.begin(),x.end()
+#define sortV(x) sort(full(x))
+#define sortVgr(x) sort(full(x),greater<LL>()) // x -> vector
+#define reverse(x) reverse(full(x)) // x -> string
+#define go(t) while(t--)
+#define endl cout<<"\n"
+#define pb push_back
+#define pob pop_back
+#define pf push_front
+#define pof pop_front
+#define mp make_pair
+#define M 1e9+7
+
+LL xxor(LL A, LL B) 
+{ 
+    LL j=0,x=0;
+    while(A || B) 
+    { 
+        if ((A & 1) && (B & 1)) x += (1 << j);
+        A >>= 1; B >>= 1; j += 1; 
+    } 
+    return x; 
+} 
+
+LL gcd(LL a, LL b)
+{
+	if(a==0) return b;
+	else return gcd(b%a,a);
+}
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+	freopen("inputCF.txt","r",stdin);
+	freopen("outputCF.txt","w",stdout);
+	#endif
+	FAST INPT OUTPT
+	LL t=1;
+	cin>>t;
+	go(t)
+	{
+		LL a,b;
+		cin>>a>>b;
+		LL p=xxor(a,b);
+		cout<< (a^p) + (b^p) <<"\n";        
+	}
+	return 0;
+}
